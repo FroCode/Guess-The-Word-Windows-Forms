1cc1845c168abe5819ff70ca5c63c78bba54f8f8
Haddouche Maamar <frocodeof@gmail.com>
2024-04-17 23:37:47 +0200

    Add ++
    ++

---
+++ Pong Game/Form1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Windows.Forms;
+
+namespace Pong_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
